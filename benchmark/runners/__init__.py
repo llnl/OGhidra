316e60f31126f5d5347a3c79a4a961c9deb6ee47
Wangmo Tenzing <tenzing1@llnl.gov>
2026-05-05 13:28:39 -0700

    add missing import

--- benchmark/runners/__init__.py
+++ benchmark/runners/__init__.py
@@ -6,10 +6,11 @@ Orchestrates benchmark execution, integrating OGhidra with ground truth
 and evaluation metrics.
 """
 
-from .benchmark_runner import BenchmarkRunner
+from .benchmark_runner import BenchmarkRunner, BenchmarkConfig
 from .oghidra_runner import OGhidraRunner
 
 __all__ = [
     "BenchmarkRunner",
+    "BenchmarkConfig",
     "OGhidraRunner",
 ]
